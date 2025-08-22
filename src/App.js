8f4874e6dfe8c64e430281f7ce01658231408b5e
P52 <dangminhquan9320@gmail.com>
2025-08-22 22:11:12 +0700

    mq

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <Routes>
           <Route path="/" element={<StartPage/>}/>
+          <Route path="/pbp" element={<StartPage/>}/>
           <Route path="/export-pdf" element={<FinalPage/>}/>
       </Routes>
     </div>
